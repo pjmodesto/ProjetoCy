ed8409a7f78ff6d79564fa1f29ad8e86629708f5
pjmodesto <info.pjmodesto@gmail.com>
2024-12-16 22:02:21 -0300

    CRUD Pos e GET

--- cypress/e2e/api/crud_posts.cy.js
+++ cypress/e2e/api/crud_posts.cy.js
@@ -1,19 +1,61 @@
 describe('CRUD - Posts', () => {
-    
+
+    let potsId = ''
+
     before(() => {
-        
+
         cy.request({
 
             method: 'POST',
             url: '/api/auth',
+            //usando variável de ambiente para logar na aplicação
+            //para isso criar o arquivo cypress.env.json
+            //também é possível criar usuário na variável de ambiente no wind - não recomentado - 
             body: {
-                email: 'testeIterasys@iterasys.com',
-                password: '123456'
+                email: Cypress.env('email'),
+                password: Cypress.env('password').toString()
             }
+        }).then(({ body }) => {
+
+            cy.setCookie('jwt', body.jwt)
+            Cypress.env('jwt', body.jwt)
+
         })
     })
 
-    it('teste', () => {
-        cy.log('teste')
+    beforeEach(() => {
+        cy.session('Login', () => {
+            cy.setCookie('jwt', Cypress.env('jwt'))
+        })
+    })//todo teste iniciado pelo cypress o cookie são limpos por padrão, isso gerar erro quando realizado teste de CRUD
+      //O Cypress.Cookies.defaults usando em aula na versão 12 do cypress foi descontinuada 
+    
+      it('cria um post', () => {
+
+        let mensagem = 'Posts via Cypress - CRUD'
+
+        cy.request({
+            method: 'POST',
+            url: '/api/posts',
+            body: {
+                text: mensagem
+            },
+            
+        }).then(({ status, body }) => {
+            expect(status).to.eq(201)
+            expect(body.text).to.eq(mensagem)
+            potsId = body._id
+        })
+    })//todo teste iniciado pelo cypress o cookie é limpo por padrão, isso gerar erro quando realizado teste de CRUD
+
+    it('lê post', () => {
+
+        cy.request({
+            method: 'GET',
+            url: `/api/posts/${potsId}`
+        }).then(({ status }) => {
+
+            expect(status).to.eq(200)
+        })
     })
 })
