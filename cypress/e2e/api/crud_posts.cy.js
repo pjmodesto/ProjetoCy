68b3095a97f90d0f6e28f95271904ca208f0dc8b
pjmodesto <info.pjmodesto@gmail.com>
2024-12-17 06:08:05 -0300

    atualiza e deleta um post

--- cypress/e2e/api/crud_posts.cy.js
+++ cypress/e2e/api/crud_posts.cy.js
@@ -1,7 +1,10 @@
 describe('CRUD - Posts', () => {
 
+    //o JScript é uma linguagem assincrona o let é uma variável JScript
     let potsId = ''
-
+    let mensagem = 'Posts via Cypress - CRUD 17/12'
+    
+    // o before é executado uma única vez
     before(() => {
 
         cy.request({
@@ -28,11 +31,9 @@ describe('CRUD - Posts', () => {
             cy.setCookie('jwt', Cypress.env('jwt'))
         })
     })//todo teste iniciado pelo cypress o cookie são limpos por padrão, isso gerar erro quando realizado teste de CRUD
-      //O Cypress.Cookies.defaults usando em aula na versão 12 do cypress foi descontinuada 
-    
-      it('cria um post', () => {
+    //O Cypress.Cookies.defaults usando em aula na versão 12 do cypress foi descontinuada 
 
-        let mensagem = 'Posts via Cypress - CRUD'
+    it('cria um post', () => {
 
         cy.request({
             method: 'POST',
@@ -40,22 +41,60 @@ describe('CRUD - Posts', () => {
             body: {
                 text: mensagem
             },
-            
+
         }).then(({ status, body }) => {
             expect(status).to.eq(201)
             expect(body.text).to.eq(mensagem)
             potsId = body._id
         })
-    })//todo teste iniciado pelo cypress o cookie é limpo por padrão, isso gerar erro quando realizado teste de CRUD
+    })//todo teste iniciado pelo cypress o estado do browser é limpo inclusive o cookie é limpo por padrão, isso gerar erro quando realizado teste de CRUD
 
     it('lê post', () => {
 
         cy.request({
             method: 'GET',
             url: `/api/posts/${potsId}`
-        }).then(({ status }) => {
+        }).then(({ status, body }) => {
 
             expect(status).to.eq(200)
+            expect(body.text).to.eq(mensagem)
+            expect(body.likes).to.have.lengthOf(0) //valida qtde do array
+        })
+    })
+
+    it('atualiza o post', () => {
+
+        cy.request({
+            method: 'PUT',
+            url: `/api/posts/like/${potsId}` //api para dar like no post
+        }).then(({ status }) => {
+            expect(status).to.eq(200)
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${potsId}`
+            }).then(({ body }) => {
+                expect(body.likes).to.have.lengthOf(1) //valida qtde do array
+            })
+        })
+    })
+
+    it('deleta post', () => {
+        
+        cy.request({
+            method: 'DELETE',
+            url: `/api/posts/${potsId}`
+        }).then(({ status, body }) =>{
+            expect(status).to.eq(200)
+            expect(body.msg).to.eq('Post removido')
+
+            cy.request({
+                method: 'GET',
+                url: `/api/posts/${potsId}`, 
+                failOnStatusCode: false //como ira retornar 404 não retornar o defaul do cypress quando retornado 404
+            }).then(({ status }) => {
+                expect(status).to.eq(404) //erro API dado não encontrado
+            })
         })
     })
 })
